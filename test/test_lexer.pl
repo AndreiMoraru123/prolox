b503211c3a0a56fde85a781f87d8721191fcd3e7
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2024-11-03 08:48:48 +0200

    test unknown token for lexer

--- test/test_lexer.pl
+++ test/test_lexer.pl
@@ -1,10 +1,18 @@
-:- initialization(run_test).
+:- initialization(test_keywords).
+:- initialization(test_tokens).
 
 :- consult('../lexer.pl').
 
-run_test :-
+test_keywords :-
     set_prolog_flag(double_quotes, codes),
     string_codes("while a == b do other ; other endwhile", Codes),
+    phrase(tokens(Z), Codes),
+    write('output: '), writeln(Z),
+    writeln('').
+
+test_tokens :-
+    set_prolog_flag(double_quotes, codes),
+    string_codes("unknown", Codes),
     phrase(tokens(Z), Codes),
     write('output: '), writeln(Z),
     writeln('').
